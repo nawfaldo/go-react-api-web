c888b268bf59590bd19ed2655a12007cab8c5893
nawfaldo <nawfaldo.main@gmail.com>
2024-07-29 19:12:44 +0700

    vercel

--- src/routes/hello.tsx
+++ src/routes/hello.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/hello")({
-  component: () => <div>Hello /hello!</div>,
+  component: () => <div>Hello!</div>,
 });
